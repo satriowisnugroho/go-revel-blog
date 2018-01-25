0d62fe638915c6d2b0f32da48ce65b7e751cf55a
Satrio Wisnugroho <wisnu@skyshi.com>
2018-01-25 19:49:00 +0700

    Add hello-world route

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -11,3 +11,13 @@ type App struct {
 func (c App) Index() revel.Result {
 	return c.Render()
 }
+
+func (c App) Hello() revel.Result {
+	data := make(map[string]interface{})
+	data["code"] = 200
+	data["status"] = true
+	data["message"] = "Hello World!"
+	data["data"] = make(map[string]interface{})
+
+	return c.RenderJSON(data)
+}
